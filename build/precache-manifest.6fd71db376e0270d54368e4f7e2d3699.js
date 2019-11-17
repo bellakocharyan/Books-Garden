d544d1d76c1a4ba2036b7cf3db94f83d8f20cbe5
Bella Kocharyan <51127816+bellakocharyan@users.noreply.github.com>
2019-11-17 21:48:33 +0400

    Add files via upload

---
+++ build/precache-manifest.6fd71db376e0270d54368e4f7e2d3699.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f16fabe8a3c007c8daa14d81fbd0a9",
+    "url": "/Team-Project/index.html"
+  },
+  {
+    "revision": "ecefe2a4a4657c4c2579",
+    "url": "/Team-Project/static/css/4.228ab2f0.chunk.css"
+  },
+  {
+    "revision": "98c63ec7fdb85ffcf33a",
+    "url": "/Team-Project/static/css/main.780a1ba8.chunk.css"
+  },
+  {
+    "revision": "50821238052be5a260cf",
+    "url": "/Team-Project/static/js/2.1d0749cc.chunk.js"
+  },
+  {
+    "revision": "e785047b74033d6b7eff",
+    "url": "/Team-Project/static/js/3.53c8a004.chunk.js"
+  },
+  {
+    "revision": "ecefe2a4a4657c4c2579",
+    "url": "/Team-Project/static/js/4.151cf6bf.chunk.js"
+  },
+  {
+    "revision": "82a6dbfb843d3e68bb69",
+    "url": "/Team-Project/static/js/5.a884f596.chunk.js"
+  },
+  {
+    "revision": "dc8893daa95ca0542300",
+    "url": "/Team-Project/static/js/6.84229f47.chunk.js"
+  },
+  {
+    "revision": "75de7c6a5375866a9fc1",
+    "url": "/Team-Project/static/js/7.18612dd0.chunk.js"
+  },
+  {
+    "revision": "008f082758796980e43e",
+    "url": "/Team-Project/static/js/8.de8e01a1.chunk.js"
+  },
+  {
+    "revision": "98c63ec7fdb85ffcf33a",
+    "url": "/Team-Project/static/js/main.9803f417.chunk.js"
+  },
+  {
+    "revision": "f88f868c693273a0ec8a",
+    "url": "/Team-Project/static/js/runtime-main.c556112a.js"
+  },
+  {
+    "revision": "da6ec07e9655df7bbb4b23ca82880d3e",
+    "url": "/Team-Project/static/media/logo.da6ec07e.png"
+  },
+  {
+    "revision": "67137dcaf8fb3d6d2cd17203f20d81b4",
+    "url": "/Team-Project/static/media/slide_1.67137dca.jpg"
+  },
+  {
+    "revision": "d27075813d4f284ff6529f08dfc08769",
+    "url": "/Team-Project/static/media/slide_2.d2707581.jpg"
+  },
+  {
+    "revision": "0736f682042aa0e40f3cfa2bc462d689",
+    "url": "/Team-Project/static/media/slide_3.0736f682.jpg"
+  }
+]);
